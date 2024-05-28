34dd9315c181d5d8e5a2f9fefefe7f02aa0d1adf
ZahraAdistiPutriAzali <zahraadisti4@gmail.com>
2024-05-28 17:35:05 +0700

    first commit

---
+++ sembako_app/routes/sembakoRoutes.js
@@ -0,0 +1,21 @@
+// routes/sembakoRoutes.js
+const express = require('express');
+const router = express.Router();
+const sembakoController = require('../controllers/sembakoController');
+
+// Dapatkan semua Sembako
+router.get('/sembako', sembakoController.getAllSembako);
+
+// Buat Sembako baru
+router.post('/sembako', sembakoController.createSembako);
+
+// Dapatkan Sembako berdasarkan ID
+router.get('/sembako/:id', sembakoController.getSembakoById);
+
+// Perbarui Sembako berdasarkan ID
+router.put('/sembako/:id', sembakoController.updateSembako);
+
+// Hapus Sembako berdasarkan ID
+router.delete('/sembako/:id', sembakoController.deleteSembako); // Perbaikan dilakukan di sini
+
+module.exports = router;
