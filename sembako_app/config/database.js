9f954be023bef3376880374489391602ac039962
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-28 18:20:30 +0700

    Delete sembako_app directory

--- sembako_app/config/database.js
+++
@@ -1,8 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('toko_sembako', 'root', '', {
-  host: '34.101.248.70',
-  dialect: 'mysql'
-});
-
-module.exports = sequelize;
