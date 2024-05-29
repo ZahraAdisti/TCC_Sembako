70206cae511b634e95fe95350a81852d2f4d1930
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-29 16:30:49 +0700

    Update sembakoController.js

--- controllers/sembakoController.js
+++ controllers/sembakoController.js
@@ -1,4 +1,4 @@
-const { Sembako } = require('../models/sembako'); // Ubah impor ini
+const { Sembako } = require('../models/sembako.js'); // Ubah impor ini
 
 // Mendapatkan semua sembako
 exports.getAllSembako = async (req, res) => {
