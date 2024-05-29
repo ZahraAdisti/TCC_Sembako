6a83edbc1301f11e0f2735318d415f9498e3d4da
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-29 16:40:27 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,7 +1,22 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('toko_sembako', 'root', '', {
-  host: '34.101.223.26', // Pastikan host ini benar
+  host: 'const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize('toko_sembako', 'root', '', {
+  host: '34.128.86.172', // Pastikan host ini benar
+  dialect: 'mysql'
+});
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Koneksi telah berhasil dibuat.');
+  })
+  .catch(err => {
+    console.error('Tidak dapat terhubung ke database:', err);
+  });
+
+module.exports = sequelize;', // Pastikan host ini benar
   dialect: 'mysql'
 });
 
