754d5c0d4655b448b303da50aafd2488d86432b9
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-29 12:34:10 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('toko_sembako', 'root', '', {
-  host: '34.101.248.70',
+  host: '34.128.86.172',
   dialect: 'mysql'
 });
 
