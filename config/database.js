74bb8d128f513236cbeb3896702f6eb65d567708
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-29 16:55:18 +0700

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,8 +1,16 @@
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize('toko_sembako', 'root', '', {
-  host: '34.128.86.172', // Pastikan host ini benar
+  host: '34.128.86.172',
   dialect: 'mysql'
 });
+
+sequelize.authenticate()
+  .then(() => {
+    console.log('Koneksi telah berhasil dibuat.');
+  })
+  .catch(err => {
+    console.error('Tidak dapat terhubung ke database:', err);
+  });
 
 module.exports = sequelize;
