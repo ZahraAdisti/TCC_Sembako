8cf4c1b59887e779cbf6e66042fe9c1b15ec1661
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-05-31 16:50:28 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const sembakoRoutes = require('./routes/sembakoRoutes');
+const path = require('path');
 const sequelize = require('./config/database');
+const sembakoRoutes = require('./routes/sembakoRoutes');
 
 const app = express();
+app.use(express.json());
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use('/api', sembakoRoutes);
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync().then(() => {
+    console.log('Database synced');
+}).catch(err => {
+    console.error('Error syncing database:', err);
+});
 
 const PORT = process.env.PORT || 3000;
-
-sequelize.sync({ force: false }).then(() => {
-    console.log('Database & tables created!');
-    app.listen(PORT, () => {
-        console.log(`Server is running on port ${PORT}`);
-    });
+app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
 });
