9cbb45c312cd95e60fcd1889a3eb79fbc3740ccc
ZahraAdistiPutriAzali <zahraadisti4@gmail.com>
2024-06-02 13:28:25 +0700

    first

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware untuk meng-handle JSON request body
+app.use(express.json());
+// Middleware untuk meng-handle form data
+app.use(express.urlencoded({ extended: false }));
+
+// Menggunakan middleware untuk menyajikan file statis
+app.use(express.static(__dirname, ));
+
+// Menangani permintaan untuk file index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname,  'index.html'));
+});
+
+// Menangani permintaan untuk file login.html
+app.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname,  'login.html'));
+});
+
+// Mulai server
+app.listen(PORT, () => {
+  console.log(`Server berjalan di http://localhost:${PORT}`);
+});
