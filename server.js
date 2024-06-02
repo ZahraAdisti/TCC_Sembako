a238d45a0a0b16d214466ed9811d09ee9e71878f
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-06-02 16:04:35 +0700

    Update server.js

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware untuk meng-handle JSON request body
+// Middleware untuk menangani JSON request body
 app.use(express.json());
-// Middleware untuk meng-handle form data
+// Middleware untuk menangani form data
 app.use(express.urlencoded({ extended: false }));
 
-// Menggunakan middleware untuk menyajikan file statis
-app.use(express.static(path.join(__dirname, 'public' ));
+// Middleware untuk menyajikan file statis
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Menangani permintaan untuk file index.html
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname,  'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Menangani permintaan untuk file login.html
 app.get('/login', (req, res) => {
-  res.sendFile(path.join(__dirname,  'login.html'));
+    res.sendFile(path.join(__dirname, 'login.html'));
 });
 
 // Mulai server
 app.listen(PORT, () => {
-  console.log(`Server berjalan di http://localhost:${PORT}`);
+    console.log(`Server berjalan di http://localhost:${PORT}`);
 });
