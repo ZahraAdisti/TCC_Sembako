9d071c722d807703a07869cfffe1c6ebbe9debdb
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-06-04 17:07:48 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,15 @@
+//app.js
 const express = require('express');
 const app = express();
-const path = require('path');
-const sequelize = require('./config/database'); // Pastikan impor dari config/database.js
 const userRoutes = require('./routes/userRoutes'); // Pastikan impor route user
-//const userRoutes = require('./routes/sembakoRoutes');
+const userRoutes = require('./routes/sembakoRoutes');
 
 // Middleware
 app.use(express.json());
 
-// Middleware for CORS
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
 // Routes
 app.use('/api/user', userRoutes); // Pastikan impor dan penggunaan routes user
-//app.use('/api', sembakoRoutes); // Pastikan impor dan penggunaan routes user
-
-
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Sync the database schema
-sequelize.sync().then(() => {
-    console.log('Database synced');
-}).catch(err => {
-    console.error('Error syncing database:', err);
-});
+app.use('/api', sembakoRoutes);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
-
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
