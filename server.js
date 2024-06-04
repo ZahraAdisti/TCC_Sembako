872d87f4b83fcb20215453f7c398666943481399
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-06-04 19:45:55 +0700

    Update server.js

--- server.js
+++ server.js
@@ -13,6 +13,14 @@ app.use(express.urlencoded({ extended: false }));
 // Menggunakan middleware untuk menyajikan file statis
 app.use(express.static(path.join(__dirname, 'public' )));
 
+// Middleware CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://fe-toko-sembako-dot-tokosembako.et.r.appspot.com');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Menangani permintaan untuk file index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,  'index.html'));
