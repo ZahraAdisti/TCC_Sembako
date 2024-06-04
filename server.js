38563b5fe11d93b9fa4e1c2feb6c9593c410bd39
ZahraAdisti <128275568+ZahraAdisti@users.noreply.github.com>
2024-06-04 20:04:33 +0700

    Update server.js

--- server.js
+++ server.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public' )));
 
 // Middleware CORS
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://fe-toko-sembako-dot-tokosembako.et.r.appspot.com');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
 });
 
 // Menangani permintaan untuk file index.html
